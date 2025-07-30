b684dabb202145c86d9066c31777624765cebdcc
mr sivasankar <jeevanantham.s@creativebees.in>
2025-07-30 17:45:37 +0530

    removed sesion

--- apicontroller/users.js
+++ apicontroller/users.js
@@ -25,9 +25,25 @@ async function user(req, res) {
  }
 }
 
+async function usercreate(req, res) {
+ try {
+    const user = await mysqlQuery(/*sql*/`INSERT INTO users (name, dob, emailId, password)
+VALUES ('Jeeva', '1990-01-01', 'jeeva@example.com', 'securepass');`)
+    if(!user) {
+        return res.status(204).send('not found')
+    }
+    res.status(200).send(user)
+ } catch (error) {
+    req.log.error(error)
+        res.status(500).json(error)
+ }
+}
+
+
 module.exports = (app) => {
 
     app.get('/users', user)
+    app.post('/user', usercreate)
 }
 
 
